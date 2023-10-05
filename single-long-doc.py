b4487fbc892ffff891942c9aafdf3b091d5d9078
vardhanam <vardhanam.daga@gmail.com>
2023-10-05 22:59:15 +0530

    Add files via upload

---
+++ single-long-doc.py
@@ -0,0 +1,39 @@
+import os
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+
+os.environ["OPENAI_API_KEY"] = "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"
+# load the document as before
+loader = PyPDFLoader('./docs/RachelGreenCV.pdf')
+documents = loader.load()
+
+# we split the data into chunks of 1,000 characters, with an overlap
+# of 200 characters between the chunks, which helps to give better results
+# and contain the context of the information between chunks
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+documents = text_splitter.split_documents(documents)
+
+# we create our vectorDB, using the OpenAIEmbeddings tranformer to create
+# embeddings from our text chunks. We set all the db information to be stored
+# inside the ./data directory, so it doesn't clutter up our source files
+vectordb = Chroma.from_documents(
+  documents,
+  embedding=OpenAIEmbeddings(),
+  persist_directory='./data'
+)
+vectordb.persist()
+
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm=OpenAI(),
+    retriever=vectordb.as_retriever(search_kwargs={'k': 7}),
+    return_source_documents=True
+)
+
+# we can now execute queries against our Q&A chain
+result = qa_chain({'query': 'Who is the CV about?'})
+print(result['result'])
